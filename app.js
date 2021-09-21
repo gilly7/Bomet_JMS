ab9f3c2525124b077e53d35d1868546b42eb7b0c
Gilbert Ngeno <45171673+gilly7@users.noreply.github.com>
2021-09-21 19:16:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//listen for submit event//
+document.getElementById('registrationform').addEventListener('submit', formSubmit);
+
+//Submit form
+function formSubmit(e) {
+  e.preventDefault();
+  // Get Values from the DOM
+  let name = document.querySelector('#name').value;
+  let email = document.querySelector('#email').value;
+  let password = document.querySelector('#password').value;
+  let bio = document.querySelector('#bio').value;
+  let job = document.querySelector('#job').value;
+  let interest = document.querySelector('#interest').value;
+   //Show Alert Message
+   document.querySelector('.alert').style.display = 'block';
+
+   //Hide Alert Message After Seven Seconds
+   setTimeout(function() {document.querySelector('.alert').style.display = 'none';
+   }, 7000);
+    //Form Reset After Submission
+   document.getElementById('registrationform').reset();
+}
